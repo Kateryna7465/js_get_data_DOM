799ce9a0436cbda6c38fbf03e109bf7cb5379b0d
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-05-17 15:48:25 +0200

    changed readme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,7 @@ let total = 0;
 for (const population of populations) {
   const text = population.textContent;
 
-  const normalized = text.replaceAll(',',  '');
+  const normalized = text.replaceAll(',', '');
 
   const number = Number(normalized);
 
@@ -17,7 +17,7 @@ for (const population of populations) {
 const average = total / populations.length;
 
 document.querySelector('.total-population').textContent =
-total.toLocaleString();
+  total.toLocaleString();
 
 document.querySelector('.avarage-population').textContent =
-Math.round(average).toLocaleString();
+  Math.round(average).toLocaleString();
